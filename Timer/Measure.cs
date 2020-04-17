0186349a7f12265e2110640aaaeab81518d7d0d9
Robert E. Bratton <rob@robbratton.com>
2020-04-17 14:52:36 -0400

    First commit

---
+++ Timer/Measure.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Timer
+{
+    public static class Measure
+    {
+        /// <summary>
+        /// Invoke an action with a return value and time it.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="action"></param>
+        /// <param name="timeSpan"></param>
+        /// <returns></returns>
+        public static T Invoke<T>(Func<T> action, out TimeSpan timeSpan)
+        {
+            var stopwatch = Stopwatch.StartNew();
+
+            var res = action.Invoke();
+
+            stopwatch.Stop();
+            timeSpan = stopwatch.Elapsed;
+
+            return res;
+        }
+
+        /// <summary>
+        /// Invoke an action without a return value and time it.
+        /// </summary>
+        /// <param name="action"></param>
+        /// <param name="timeSpan"></param>
+        public static void Invoke(Action action, out TimeSpan timeSpan)
+        {
+            var stopwatch = Stopwatch.StartNew();
+
+            action.Invoke();
+
+            stopwatch.Stop();
+            timeSpan = stopwatch.Elapsed;
+        }
+
+        /// <summary>
+        /// Invoke an action and time it returning the execution time.
+        /// </summary>
+        /// <param name="action"></param>
+        public static TimeSpan Invoke(Action action)
+        {
+            var stopwatch = Stopwatch.StartNew();
+
+            action.Invoke();
+
+            stopwatch.Stop();
+            return stopwatch.Elapsed;
+        }
+
+    }
+}
---
+++ Timer/StopClock.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using Microsoft.Win32.SafeHandles;
+
+namespace Timer
+{
+    /// <summary>
+    /// Time operations
+    /// </summary>
+    /// <example>
+    /// using (new StopClock(Console.OpenStandardError, "Optional Title")){
+    ///     (Do stuff)
+    /// }
+    /// at the close of the using, the elapsed time will be written to the stream.
+    /// </example>.
+    public class StopClock : IDisposable
+    {
+        private readonly Stopwatch _stopwatch;
+        private readonly string _title;
+        private readonly StreamWriter _streamWriter;
+        private bool _disposed;
+        private readonly SafeHandle _handle = new SafeFileHandle(IntPtr.Zero, true);
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="stream">Stream for output.</param>
+        /// <param name="title">Title</param>
+        /// <remarks>You can use Console.OpenStandardOutput or Console.OpenStandardError, etc.</remarks>
+        public StopClock(Stream stream, string title = null)
+        {
+            if (stream == null)
+            {
+                throw new ArgumentNullException(nameof(stream));
+            }
+
+            _streamWriter = new StreamWriter(stream);
+            _title = title;
+            _stopwatch = Stopwatch.StartNew();
+        }
+
+        /// <summary>
+        /// Stop
+        /// </summary>
+        /// <returns></returns>
+        public Stopwatch Stop()
+        {
+            _stopwatch.Stop();
+
+            return _stopwatch;
+        }
+
+        /// <summary>
+        /// Gets the current elapsed time.
+        /// </summary>
+        /// <remarks>Does not stop the stopwatch.</remarks>
+        public TimeSpan Elapsed => _stopwatch.Elapsed;
+
+        /// <summary>
+        /// Print the current elapsed time.
+        /// </summary>
+        public void Print()
+        {
+            if (_streamWriter != null)
+            {
+                var title = "";
+                if (_title != null)
+                {
+                    title = _title + ": ";
+                }
+
+                _streamWriter.WriteLine($"{title}Duration: {_stopwatch.Elapsed}");
+            }
+        }
+
+        /// <summary>
+        /// Dispose
+        /// </summary>
+        public void Dispose()
+        {
+            // Dispose of unmanaged resources.
+            Dispose(true);
+            // Suppress finalization.
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Overridable Dispose
+        /// </summary>
+        /// <param name="disposing"></param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed)
+                return; 
+      
+            if (disposing) {
+                _handle.Dispose();
+
+                Stop();
+                Print();
+            }
+      
+            _disposed = true;
+        }
+
+        /// <summary>
+        /// Destructor
+        /// </summary>
+        ~StopClock()
+        {
+            Dispose(false);
+        }
+    }
+}
